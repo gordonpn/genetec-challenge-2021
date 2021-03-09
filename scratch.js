9bc5e17777532a7165705a2adf160e2eeb5f833f
Sujan Saravanamuthu <sujansaravanamuthu@gmail.com>
2021-03-09 08:46:46 -0500

    time

--- scratch.js
+++ scratch.js
@@ -1,14 +1,21 @@
-const receivedTime = Date.parse("2021-03-09T00:59:23");
-const FIVE_HOURS = 18000;
+const receivedTime = Date.parse("2021-03-09T13:41:00");
+const FIVE_HOURS = 18000000;
 const convertedTime = receivedTime - FIVE_HOURS;
 console.log("file: scratch.js | line 4 | convertedTime", convertedTime);
 const timeNow = new Date().getTime();
 console.log("file: scratch.js | line 6 | timeNow", timeNow);
 const diffInMilliseconds = Math.abs(timeNow - convertedTime);
 
-if (diffInMilliseconds >= 120000) {
+if (diffInMilliseconds <= 120000) {
   console.log(
     "file: scratch.js | line 10 | diffInMilliseconds >= 120",
-    diffInMilliseconds >= 120
+    diffInMilliseconds <= 120000, diffInMilliseconds, 'Good'
+  );
+}
+
+else if (diffInMilliseconds >= 120000) {
+  console.log(
+    "file: scratch.js | line 10 | diffInMilliseconds >= 120",
+    diffInMilliseconds >= 120000, diffInMilliseconds, 'expired'
   );
 }
