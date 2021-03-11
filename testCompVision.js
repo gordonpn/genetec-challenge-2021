34118639a823792a2ad4ff89d34d9e650e5430fe
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-03-11 10:53:14 -0500

    feat: retry if ocr failed

--- testCompVision.js
+++ testCompVision.js
@@ -20,9 +20,8 @@ const formatAndValidatePlate = (plate) => {
   // only return valid length plates
   if (newPlate.length >= 6 && newPlate.length <= 7) {
     return newPlate;
-  } else {
-    return null;
   }
+  return null;
 };
 
 const getImageToText = async (url) => {
@@ -35,14 +34,14 @@ const getImageToText = async (url) => {
 
     // loop through the text values found and format and val
     data.recognitionResult.lines.forEach((line) => {
-      const formattedPlate = formatAndValidatePlate(line["text"]);
+      const formattedPlate = formatAndValidatePlate(line.text);
 
       // the plate is either null or a valid string here
       if (formattedPlate) {
         console.log(`we need to validate this plate ${formattedPlate}`);
         // if the formatted plate isn't null then we can check the repo and eventually send the post request
       } else {
-        console.log(`Skipping this invalid text ${line["text"]}`);
+        console.log(`Skipping this invalid text ${line.text}`);
       }
     });
   } catch (err) {
@@ -50,31 +49,45 @@ const getImageToText = async (url) => {
   }
 };
 
-const poll = async (url) => {
+const pollingSuccess = async (url) => {
   // status could be Failed, Succeeded, Running
   let pollingResponse = await axios.get(url, { headers: headers });
 
-  while (pollingResponse?.data?.status !== "Succeeded") {
+  while (
+    pollingResponse?.data?.status !== "Succeeded" &&
+    pollingResponse?.data?.status !== "Failed"
+  ) {
     console.log(`Operation status: ${pollingResponse?.data?.status}`);
     await sleeper(200)();
     pollingResponse = await axios.get(url, { headers: headers });
+  }
+
+  if (pollingResponse?.data?.status === "Succeeded") {
+    return true;
+  } else if (pollingResponse?.data?.status === "Failed") {
+    return false;
   }
 };
 
 const postImage = async (url) => {
   try {
-    const res = await axios.post(
-      postUrl,
-      { url },
-      {
-        headers: headers,
-      }
-    );
-    await poll(res.headers["operation-location"]);
+    let requestSuccess = false;
+    let res;
+
+    while (!requestSuccess) {
+      res = await axios.post(
+        postUrl,
+        { url },
+        {
+          headers: headers,
+        }
+      );
+      requestSuccess = await pollingSuccess(res.headers["operation-location"]);
+    }
     return await getImageToText(res.headers["operation-location"]);
   } catch (err) {
     console.log(err?.response);
   }
 };
 
-postImage(testImageUrl).then();
+postImage(testImageUrl).catch((err) => console.log(err));
